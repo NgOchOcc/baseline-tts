1e4b72ab8b2b94722d3bee2e2e0a923c0f5af1cc
ngOchOc <vanlangoc2002@gmail.com>
2026-03-09 23:33:37 +0700

    add script run method

--- src/envs/__init__.py
+++ src/envs/__init__.py
@@ -6,7 +6,7 @@ from importlib import import_module
 
 
 def get_env_datasets(env_name: str, **kwargs):
-    if env_name == 'AMC23' or env_name == 'AIME24':
+    if env_name in ['AMC23', 'AIME24', 'MINERVA']:
         task_module = import_module(f"envs.MATH")
     else:
         task_module = import_module(f"envs.{env_name}")
@@ -14,7 +14,10 @@ def get_env_datasets(env_name: str, **kwargs):
 
 
 def get_default_query_str_builder(env_name: str, **kwargs):
-    task_module = import_module(f"envs.{env_name}")
+    if env_name in ['AMC23', 'AIME24', 'MINERVA']:
+        task_module = import_module(f"envs.MATH")
+    else:
+        task_module = import_module(f"envs.{env_name}")
 
     def fn(problem_input: str, is_few_shot: bool, model_names: list):
         return task_module.Env.build_query_str(
