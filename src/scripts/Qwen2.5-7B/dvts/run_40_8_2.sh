6b95db53708ee896032fe3504dbcf18b1e57a16e
ngOchOc <vanlangoc2002@gmail.com>
2026-03-09 23:31:06 +0700

    add script run method

--- src/scripts/Qwen2.5-7B/dvts/run_40_8_2.sh
+++ src/scripts/Qwen2.5-7B/dvts/run_40_8_2.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+export VALUE_MODEL_PATH=Qwen/Qwen2.5-Math-PRM-7B
+export POLICY_MODEL_PATH=Qwen/Qwen2.5-7B-Instruct
+export LOGDIR=logs/cot_math
+export HOST_ADDR=0.0.0.0
+export CONTROLLER_PORT=10014
+export WORKER_BASE_PORT=10081
+export PYTHONPATH=$(pwd)
+
+cd ${PYTHONPATH}
+save_dir=${PYTHONPATH}/output
+LOGDIR=${PYTHONPATH}/logs_fastchat
+controller_addr=http://$HOST_ADDR:$CONTROLLER_PORT
+
 
 LM=Qwen/Qwen2.5-Math-PRM-7B
 RM=Qwen/Qwen2.5-7B-Instruct
@@ -14,13 +27,13 @@ batch_size=500
 max_time=3
 double_line_break=1
 local=0
-
+num_worker=1
 
 for task in $task_names; do
     python reason/evaluation/evaluate.py \
         --LM $POLICY_MODEL_PATH \
         --RM $VALUE_MODEL_PATH \
-        --task_name $task_name \
+        --task_name $task \
         --temperature $temperature \
         --max_new_tokens $max_new_tokens \
         --num_sequence $num_sequence \
